009eb19855ab4ac307d88fe421dc37211274e3d0
Joao <jplofield@gmail.com>
2023-11-11 19:00:02 -0300

    add readme

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const img = document.querySelector("#profile img")
 
 if(html.classList.contains("light")){
 
-  img.setAttribute("src", "./assets/MIM DE light.jpg")
+  img.setAttribute("src", "./assets/avatar-light.png")
 
 }else{
 
-img.setAttribute("src", "./assets/MIM DE.jpg")
+img.setAttribute("src", "./assets/avatar.png")
 
 }
 
